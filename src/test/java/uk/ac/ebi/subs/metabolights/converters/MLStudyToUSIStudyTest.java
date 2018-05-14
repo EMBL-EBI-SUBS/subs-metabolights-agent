4622aa5a316d9c0b775edc46d3bb7edc3f40e60b
Kalai <>
2018-05-14 13:45:04 +0100

    ML-USI study converter unit test added

--- src/test/java/uk/ac/ebi/subs/metabolights/converters/MLStudyToUSIStudyTest.java
+++ src/test/java/uk/ac/ebi/subs/metabolights/converters/MLStudyToUSIStudyTest.java
@@ -1,8 +1,10 @@
 package uk.ac.ebi.subs.metabolights.converters;
 
 import org.junit.Test;
+import uk.ac.ebi.subs.metabolights.model.Contact;
+import uk.ac.ebi.subs.metabolights.model.Study;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
 /**
  * Created by kalai on 19/12/2017.
@@ -10,6 +12,12 @@ import static org.junit.Assert.*;
 public class MLStudyToUSIStudyTest {
     @Test
     public void convert() throws Exception {
+        Study mlStudyFromDisc = WSUtils.getMLStudyFromDisc();
+        List<Contact> people = mlStudyFromDisc.getPeople();
+        for(Contact contact : people){
+            System.out.println(contact.getFirstName());
+        }
+
 
     }
 
