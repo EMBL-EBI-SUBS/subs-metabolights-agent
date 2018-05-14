64f259e193d217d768826a9dec3752b6d66f55a4
Kalai <>
2018-05-14 13:38:32 +0100

    USI-ML and ML-USI descriptor converter added

---
+++ src/main/java/uk/ac/ebi/subs/metabolights/converters/MLDescriptorToUSIDescriptor.java
@@ -0,0 +1,20 @@
+package uk.ac.ebi.subs.metabolights.converters;
+
+import org.springframework.core.convert.converter.Converter;
+import uk.ac.ebi.subs.data.component.Attribute;
+import uk.ac.ebi.subs.data.component.Term;
+import uk.ac.ebi.subs.metabolights.model.OntologyModel;
+
+import java.util.Arrays;
+
+public class MLDescriptorToUSIDescriptor implements Converter<OntologyModel, Attribute> {
+    @Override
+    public Attribute convert(OntologyModel source) {
+        Attribute attribute = new Attribute();
+        attribute.setValue(source.getAnnotationValue());
+        Term term = new Term();
+        term.setUrl(source.getTermAccession());
+        attribute.setTerms(Arrays.asList(term));
+        return attribute;
+    }
+}
