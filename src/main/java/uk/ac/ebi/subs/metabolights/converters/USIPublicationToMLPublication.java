307df358faca3b8416a24fafb0d082fc3fa7893a
Kalai <>
2018-05-14 13:37:13 +0100

    USI-ML publication converter added

--- src/main/java/uk/ac/ebi/subs/metabolights/converters/USIPublicationToMLPublication.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/converters/USIPublicationToMLPublication.java
@@ -2,23 +2,22 @@ package uk.ac.ebi.subs.metabolights.converters;
 
 import org.springframework.core.convert.converter.Converter;
 import uk.ac.ebi.subs.data.component.Publication;
-import uk.ac.ebi.subs.metabolights.model.CharacteristicType;
 import uk.ac.ebi.subs.metabolights.model.OntologyModel;
 
 /**
  * Created by kalai on 13/12/2017.
  */
-public class USIPublicationToMLPublication implements Converter<Publication, uk.ac.ebi.subs.metabolights.model.StudyPublication> {
+public class USIPublicationToMLPublication implements Converter<Publication, uk.ac.ebi.subs.metabolights.model.Publication> {
     @Override
-    public uk.ac.ebi.subs.metabolights.model.StudyPublication convert(Publication source) {
+    public uk.ac.ebi.subs.metabolights.model.Publication convert(Publication source) {
         if(source == null)return null;
-        uk.ac.ebi.subs.metabolights.model.StudyPublication publication = new uk.ac.ebi.subs.metabolights.model.StudyPublication();
+        uk.ac.ebi.subs.metabolights.model.Publication publication = new uk.ac.ebi.subs.metabolights.model.Publication();
         publication.setDoi(source.getDoi());
         publication.setAuthorList(source.getAuthors());
         publication.setTitle(source.getArticleTitle());
         publication.setPubMedID(source.getPubmedId());
         if(source.getStatus()!=null){
-            CharacteristicType status = new CharacteristicType();
+            OntologyModel status = new OntologyModel();
             status.setAnnotationValue(source.getStatus().name());
             publication.setStatus(status);
         }
