a61ea5248ad096f667eb1781503dbce7a4d7c113
kalaij <1823024+kalaij@users.noreply.github.com>
2018-07-16 17:48:27 +0100

    protocol content validation added

--- src/main/java/uk/ac/ebi/subs/metabolights/validator/StudyValidator.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/validator/StudyValidator.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.subs.data.component.Contact;
 import uk.ac.ebi.subs.data.submittable.Project;
+import uk.ac.ebi.subs.data.submittable.Protocol;
 import uk.ac.ebi.subs.validator.data.SingleValidationResult;
 import uk.ac.ebi.subs.validator.data.StudyValidationMessageEnvelope;
 import uk.ac.ebi.subs.validator.data.structures.SingleValidationResultStatus;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -20,6 +22,7 @@ public class StudyValidator {
         List<SingleValidationResult> validationResults = new ArrayList<>();
 
         validationResults.addAll(validateContacts(envelope.getProject().getBaseSubmittable()));
+        validationResults.addAll(validateProtocols(envelope.getProtocols()));
 
         return validationResults;
     }
@@ -36,6 +39,27 @@ public class StudyValidator {
           }
        }
        return contactValidations;
+    }
+
+    private List<SingleValidationResult> validateProtocols(List<Protocol> protocols){
+        List<SingleValidationResult>  protocolValidations = new ArrayList<>();
+        if(protocols!= null && !protocols.isEmpty()){
+            for(Protocol protocol : protocols){
+                String protocolName = protocol.getTitle();
+                if(protocol.getDescription() == null || protocol.getDescription().isEmpty()){
+                    protocolValidations.add(ValidationUtils.generateSingleValidationResult(protocol, "Protocol " +
+                            protocolName +
+                            " has no description provided", SingleValidationResultStatus.Error));
+                }  else{
+                    if(!ValidationUtils.minCharRequirementPassed(protocol.getDescription(),3)){
+                        protocolValidations.add(ValidationUtils.generateSingleValidationResult(protocol, "Protocol " +
+                                protocolName +
+                                " description is not sufficient", SingleValidationResultStatus.Error));
+                    }
+                }
+            }
+        }
+        return protocolValidations;
     }
 
 }
