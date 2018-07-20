bd657b13d89eb0b18662219d0e5f8cd828b7eb63
kalaij <1823024+kalaij@users.noreply.github.com>
2018-07-20 17:14:40 +0100

    ML to USI study converter test modified

--- src/test/java/uk/ac/ebi/subs/metabolights/converters/MLStudyToUSIStudyTest.java
+++ src/test/java/uk/ac/ebi/subs/metabolights/converters/MLStudyToUSIStudyTest.java
@@ -1,10 +1,13 @@
 package uk.ac.ebi.subs.metabolights.converters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import uk.ac.ebi.subs.metabolights.model.Contact;
 import uk.ac.ebi.subs.metabolights.model.Study;
 
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by kalai on 19/12/2017.
@@ -13,12 +16,8 @@ public class MLStudyToUSIStudyTest {
     @Test
     public void convert() throws Exception {
         Study mlStudyFromDisc = WSUtils.getMLStudyFromDisc();
-        List<Contact> people = mlStudyFromDisc.getPeople();
-        for(Contact contact : people){
-            System.out.println(contact.getFirstName());
-        }
-
-
+        MLStudyToUSIStudy mlStudyToUSIStudy = new MLStudyToUSIStudy();
+        uk.ac.ebi.subs.data.submittable.Study usiStudy = mlStudyToUSIStudy.convert(mlStudyFromDisc);
+        assertEquals(usiStudy.getAttributes().size(), 2);
     }
-
 }
