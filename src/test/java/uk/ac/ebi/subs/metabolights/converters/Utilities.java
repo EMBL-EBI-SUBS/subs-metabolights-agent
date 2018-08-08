aeedb78ae49e9879be3ee6b7d56fb2994aef9a8c
kalaij <1823024+kalaij@users.noreply.github.com>
2018-08-08 15:50:17 +0100

    utility methods refactored

--- src/test/java/uk/ac/ebi/subs/metabolights/converters/Utilities.java
+++ src/test/java/uk/ac/ebi/subs/metabolights/converters/Utilities.java
@@ -1,8 +1,16 @@
 package uk.ac.ebi.subs.metabolights.converters;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.commons.io.IOUtils;
 import uk.ac.ebi.subs.data.component.*;
 import uk.ac.ebi.subs.data.submittable.*;
+import uk.ac.ebi.subs.processing.SubmissionEnvelope;
+import uk.ac.ebi.subs.validator.model.Submittable;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.*;
@@ -11,6 +19,8 @@ import java.util.*;
  * Created by kalai on 19/12/2017.
  */
 public class Utilities {
+    public static final ObjectMapper mapper = new ObjectMapper();
+
     public static uk.ac.ebi.subs.metabolights.model.Contact generateMLContact() {
         uk.ac.ebi.subs.metabolights.model.Contact contact = new uk.ac.ebi.subs.metabolights.model.Contact();
         contact.setAddress("Hinxton");
@@ -106,6 +116,122 @@ public class Utilities {
         usiRelationship.setRelationshipNature("Child of");
         usiRelationship.setAccession("SAM990");
         return usiRelationship;
+    }
+
+
+    public static uk.ac.ebi.subs.metabolights.model.Study getMLStudyFromDisc() {
+        uk.ac.ebi.subs.metabolights.model.Study study = new uk.ac.ebi.subs.metabolights.model.Study();
+        try {
+            String result = IOUtils.toString(WSUtils.class.getClassLoader().getResourceAsStream("Test_json/MTBLS2_isa.json"));
+            uk.ac.ebi.subs.metabolights.model.Project project = new uk.ac.ebi.subs.metabolights.model.Project();
+            try {
+                mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+                project = mapper.readValue(result, uk.ac.ebi.subs.metabolights.model.Project.class);
+                study = project.getStudies().get(0);
+                return study;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return study;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static uk.ac.ebi.subs.metabolights.model.Project getMLProjectFromDisc() {
+        uk.ac.ebi.subs.metabolights.model.Project project = new uk.ac.ebi.subs.metabolights.model.Project();
+        try {
+            String result = IOUtils.toString(WSUtils.class.getClassLoader().getResourceAsStream("Test_json/MTBLS2_isa.json"));
+            try {
+                mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+                project = mapper.readValue(result, uk.ac.ebi.subs.metabolights.model.Project.class);
+                return project;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static SubmissionEnvelope getUSISubmisisonFromDisc() {
+        SubmissionEnvelope submissionEnvelope = new SubmissionEnvelope();
+        try {
+            String result = IOUtils.toString(WSUtils.class.getClassLoader().getResourceAsStream("Test_json/MTBLS2_usi.json"));
+            try {
+                mapper.registerModule(new JavaTimeModule());
+                submissionEnvelope = mapper.readValue(result, SubmissionEnvelope.class);
+                return submissionEnvelope;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static uk.ac.ebi.subs.metabolights.model.Sample getMLSampleFromDisc() {
+        uk.ac.ebi.subs.metabolights.model.Sample sample;
+        try {
+            String result = IOUtils.toString(WSUtils.class.getClassLoader().getResourceAsStream("Test_json/ML_Single_Sample.json"));
+            try {
+                mapper.registerModule(new JavaTimeModule());
+                sample = mapper.readValue(result, uk.ac.ebi.subs.metabolights.model.Sample.class);
+                return sample;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static uk.ac.ebi.subs.data.submittable.Sample getUSISampleFromDisc() {
+        uk.ac.ebi.subs.data.submittable.Sample sample;
+        try {
+            String result = IOUtils.toString(WSUtils.class.getClassLoader().getResourceAsStream("Test_json/USI_Single_Sample.json"));
+            try {
+                mapper.registerModule(new JavaTimeModule());
+                sample = mapper.readValue(result, uk.ac.ebi.subs.data.submittable.Sample.class);
+                return sample;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<uk.ac.ebi.subs.validator.model.Submittable<Sample>> getUSISampleListFromDisc() {
+        List<uk.ac.ebi.subs.data.submittable.Sample> samples;
+        List<uk.ac.ebi.subs.validator.model.Submittable<Sample>> submittableSamples = new ArrayList<>();
+        try {
+            String result = IOUtils.toString(WSUtils.class.getClassLoader().getResourceAsStream("Test_json/MTBLS2_usi_sampleList.json"));
+            try {
+                mapper.registerModule(new JavaTimeModule());
+                samples = mapper.readValue(result, new TypeReference<List<Sample>>(){});
+                for(uk.ac.ebi.subs.data.submittable.Sample sample : samples){
+                    submittableSamples.add(new uk.ac.ebi.subs.validator.model.Submittable<>(sample,"1"));
+                }
+                return submittableSamples;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static uk.ac.ebi.subs.validator.model.Submittable<Study> getUSIStudyFromDisc() {
+        SubmissionEnvelope submissionEnvelope = getUSISubmisisonFromDisc();
+        List<uk.ac.ebi.subs.data.submittable.Study> studies = submissionEnvelope.getStudies();
+        return new Submittable<>(studies.get(0),submissionEnvelope.getSubmission().getId());
     }
 
 
