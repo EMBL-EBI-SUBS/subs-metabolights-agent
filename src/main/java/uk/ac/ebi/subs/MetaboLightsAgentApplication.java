03a158ec90b01f72a87493a1112959eb7183a69a
kalaij <1823024+kalaij@users.noreply.github.com>
2018-08-22 16:07:35 +0100

    main class refactored

--- src/main/java/uk/ac/ebi/subs/MetaboLightsAgentApplication.java
+++ src/main/java/uk/ac/ebi/subs/MetaboLightsAgentApplication.java
@@ -1,21 +1,14 @@
 package uk.ac.ebi.subs;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import uk.ac.ebi.subs.metabolights.services.MLProperties;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableConfigurationProperties
 @SpringBootApplication(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MetaboLightsAgentApplication {
 
@@ -24,9 +17,6 @@ public class MetaboLightsAgentApplication {
         ApplicationPidFileWriter applicationPidFileWriter = new ApplicationPidFileWriter();
         springApplication.addListeners( applicationPidFileWriter );
         springApplication.run(args);
-        System.out.println("Args = " + args[0].toString());
-        MLProperties mlProperties = new MLProperties();
-        System.out.println("Values " + mlProperties.getApiKey());
     }
 
     @Bean
