9229014849b1cf3b74d375d387ff9751a124559b
kalaij <1823024+kalaij@users.noreply.github.com>
2018-08-22 21:46:44 +0100

    Method call added to create new study and fetch accession

--- src/main/java/uk/ac/ebi/subs/metabolights/services/FetchService.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/services/FetchService.java
@@ -1,7 +1,14 @@
 package uk.ac.ebi.subs.metabolights.services;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +26,9 @@ public class FetchService {
     private static final String METABOLIGHTS_API = "http://ves-ebi-90:5000/metabolights/ws/studies/";
 
     private RestTemplate restTemplate;
+
+    @Autowired
+    private MLProperties mlProperties;
 
 
     public FetchService() {
@@ -43,6 +53,24 @@ public class FetchService {
             throw new RestClientException(e.getMessage(), e);
         }
         return null;
+    }
+
+    public String createNewStudyAndGetAccession() {
+        String accession;
+        try {
+            String endPoint = METABOLIGHTS_API + "create_study";
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("user_token", mlProperties.getApiKey());
+
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+            JsonNode result = restTemplate.postForObject(endPoint, entity, ObjectNode.class);
+            accession = result.path("new_study").asText();
+            return accession;
+
+        } catch (RestClientException e) {
+            throw new RestClientException(e.getMessage(), e);
+        }
     }
 
 }
