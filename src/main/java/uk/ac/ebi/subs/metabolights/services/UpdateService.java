43962ec9545e23a5da8d95f4e0e4683af9f79c31
kalaij <1823024+kalaij@users.noreply.github.com>
2018-08-29 17:11:53 +0100

    Update service added with method to update existing contact

--- src/main/java/uk/ac/ebi/subs/metabolights/services/UpdateService.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/services/UpdateService.java
@@ -1,13 +1,22 @@
 package uk.ac.ebi.subs.metabolights.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.omg.CORBA.Object;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.subs.data.component.Contact;
 import uk.ac.ebi.subs.metabolights.converters.USIContactsToMLContacts;
@@ -20,8 +29,6 @@ import java.util.List;
 public class UpdateService {
 
     private static final Logger logger = LoggerFactory.getLogger(FetchService.class);
-
-    private static final String METABOLIGHTS_API = "http://ves-ebi-90:5000/metabolights/ws/studies/";
 
     private RestTemplate restTemplate;
 
@@ -37,24 +44,35 @@ public class UpdateService {
         this.restTemplate = new RestTemplate();
         List messageConverters = this.restTemplate.getMessageConverters();
         messageConverters.add(new JsonAsTextPlainHttpMessageConverter());
+
+        MappingJackson2HttpMessageConverter jsonHttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        jsonHttpMessageConverter.getObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        messageConverters.add(jsonHttpMessageConverter);
         this.restTemplate.setMessageConverters(messageConverters);
+
         usiContactsToMLContacts = new USIContactsToMLContacts();
+        mlProperties = new MLProperties();
     }
 
-    public void updateContacts(String studyID, List<Contact> contacts) {
-        if (contacts == null || contacts.isEmpty()) return;
-        JSONObject json = new JSONObject();
+    public void updateContact(String studyID, Contact contact) {
+        if (contact == null) return;
+        if (contact.getEmail() == null) return;
+
         try {
-            json.put("contacts" , usiContactsToMLContacts.convert(contacts));
-        } catch (JSONException e) {
+            ObjectNode contactToUpdateJSON = ServiceUtils.convertToJSON(usiContactsToMLContacts.convert(contact), "contact");
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("user_token", mlProperties.getApiKey());
+
+            HttpEntity<ObjectNode> requestBody = new HttpEntity<>(contactToUpdateJSON, headers);
+            String url = mlProperties.getUrl() + studyID + "/contacts?email=" + contact.getEmail();
+            restTemplate.put(url, requestBody, new Object[]{});
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("user_token", mlProperties.getApiKey());
-
-        HttpEntity<JSONObject> requestBody = new HttpEntity<>(json, headers);
-        restTemplate.put(mlProperties.getUrl() + studyID + "/contacts", requestBody, new Object[] {});
-
     }
+
+
 }
