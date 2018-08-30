16c3ce7adb4dd8638f1ca2cd95a47f34de78d37d
kalaij <1823024+kalaij@users.noreply.github.com>
2018-08-30 16:15:32 +0100

    Update existing publication method added

--- src/main/java/uk/ac/ebi/subs/metabolights/services/UpdateService.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/services/UpdateService.java
@@ -20,6 +20,8 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.subs.data.component.Contact;
 import uk.ac.ebi.subs.metabolights.converters.USIContactsToMLContacts;
+import uk.ac.ebi.subs.metabolights.converters.USIPublicationToMLPublication;
+import uk.ac.ebi.subs.metabolights.model.Publication;
 import uk.ac.ebi.subs.metabolights.model.Study;
 import uk.ac.ebi.subs.metabolights.validator.schema.custom.JsonAsTextPlainHttpMessageConverter;
 
@@ -34,10 +36,13 @@ public class UpdateService {
 
     private MLProperties mlProperties;
 
+    private  HttpHeaders headers;
+
     @Autowired
     private FetchService fetchService;
 
     private USIContactsToMLContacts usiContactsToMLContacts;
+    private USIPublicationToMLPublication usiPublicationToMLPublication;
 
 
     public UpdateService() {
@@ -52,7 +57,11 @@ public class UpdateService {
         this.restTemplate.setMessageConverters(messageConverters);
 
         usiContactsToMLContacts = new USIContactsToMLContacts();
+        usiPublicationToMLPublication = new USIPublicationToMLPublication();
         mlProperties = new MLProperties();
+
+        headers = new HttpHeaders();
+        headers.set("user_token", mlProperties.getApiKey());
     }
 
     public void updateContact(String studyID, Contact contact) {
@@ -61,11 +70,24 @@ public class UpdateService {
 
         try {
             ObjectNode contactToUpdateJSON = ServiceUtils.convertToJSON(usiContactsToMLContacts.convert(contact), "contact");
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("user_token", mlProperties.getApiKey());
-
             HttpEntity<ObjectNode> requestBody = new HttpEntity<>(contactToUpdateJSON, headers);
             String url = mlProperties.getUrl() + studyID + "/contacts?email=" + contact.getEmail();
+            restTemplate.put(url, requestBody, new Object[]{});
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void updatePublication(String studyID, uk.ac.ebi.subs.data.component.Publication publication) {
+        if (publication == null) return;
+        if (publication.getArticleTitle() == null) return;
+
+        try {
+            ObjectNode contactToUpdateJSON = ServiceUtils.convertToJSON(usiPublicationToMLPublication.convert(publication), "publication");
+            HttpEntity<ObjectNode> requestBody = new HttpEntity<>(contactToUpdateJSON, headers);
+            String url = mlProperties.getUrl() + studyID + "/publications?title=" + publication.getArticleTitle();
             restTemplate.put(url, requestBody, new Object[]{});
 
         } catch (Exception e) {
