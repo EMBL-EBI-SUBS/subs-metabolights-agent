67f9d3a3f9ada2e6094e455ec92a61840dc21e03
kalaij <1823024+kalaij@users.noreply.github.com>
2018-09-27 23:58:39 +0100

    rest template message converters added

--- src/main/java/uk/ac/ebi/subs/MetaboLightsAgentApplication.java
+++ src/main/java/uk/ac/ebi/subs/MetaboLightsAgentApplication.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.subs;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import uk.ac.ebi.subs.metabolights.validator.schema.custom.JsonAsTextPlainHttpMessageConverter;
+
+import java.util.List;
 
 @SpringBootApplication(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MetaboLightsAgentApplication {
@@ -21,6 +26,15 @@ public class MetaboLightsAgentApplication {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        RestTemplate restTemplate = builder.build();
+        List messageConverters = restTemplate.getMessageConverters();
+        messageConverters.add(new JsonAsTextPlainHttpMessageConverter());
+
+        MappingJackson2HttpMessageConverter jsonHttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        jsonHttpMessageConverter.getObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        messageConverters.add(jsonHttpMessageConverter);
+        restTemplate.setMessageConverters(messageConverters);
+        return restTemplate;
     }
 }
