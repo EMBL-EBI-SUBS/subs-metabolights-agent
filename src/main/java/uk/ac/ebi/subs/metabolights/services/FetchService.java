1c4fedfe6978e5e42a45c5f02e1008090d51f52b
kalaij <1823024+kalaij@users.noreply.github.com>
2018-09-28 16:18:02 +0100

    minor refactoring

--- src/main/java/uk/ac/ebi/subs/metabolights/services/FetchService.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/services/FetchService.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
--- src/test/java/uk/ac/ebi/subs/metabolights/services/PostServiceTest.java
+++ src/test/java/uk/ac/ebi/subs/metabolights/services/PostServiceTest.java
@@ -1,6 +1,6 @@
 package uk.ac.ebi.subs.metabolights.services;
 
-import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
