40cc5ec12a45097061a31c7f55d50c2dedecca11
kalaij <1823024+kalaij@users.noreply.github.com>
2018-10-11 23:14:11 +0100

    Study processor using mocks

--- src/test/java/uk/ac/ebi/subs/metabolights/agent/MockMetaboLightsStudyProcessorTest.java
+++ src/test/java/uk/ac/ebi/subs/metabolights/agent/MockMetaboLightsStudyProcessorTest.java
@@ -1,4 +1,52 @@
 package uk.ac.ebi.subs.metabolights.agent;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import uk.ac.ebi.subs.data.Submission;
+import uk.ac.ebi.subs.data.submittable.Project;
+import uk.ac.ebi.subs.data.submittable.Study;
+import uk.ac.ebi.subs.metabolights.services.FetchService;
+import uk.ac.ebi.subs.metabolights.validator.ValidationTestUtils;
+import uk.ac.ebi.subs.processing.ProcessingCertificateEnvelope;
+import uk.ac.ebi.subs.processing.SubmissionEnvelope;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MockMetaboLightsStudyProcessorTest {
+
+    @Mock
+    FetchService fetchServiceMock;
+
+    @Mock
+    ProcessingCertificateGenerator processingCertificateGenerator;
+
+    @Mock
+    RabbitMessagingTemplate rabbitMessagingTemplate;
+
+    @InjectMocks
+    MetaboLightsStudyProcessor metaboLightsStudyProcessorMock;
+
+
+    @Test
+    public void testCreateNewStudyAndGetAccessionWithMock() {
+        when(fetchServiceMock.createNewStudyAndGetAccession()).thenReturn("MTBLS_DEV122");
+        SubmissionEnvelope submissionEnvelope = new SubmissionEnvelope();
+        Submission submission = new Submission();
+        submission.setId("test-0");
+        submissionEnvelope.setSubmission(submission);
+        Study study = new Study();
+        Project project = ValidationTestUtils.getProjectWithContactsAndPublications().getBaseSubmittable();
+        submissionEnvelope.getStudies().add(study);
+        submissionEnvelope.getProjects().add(project);
+        ProcessingCertificateEnvelope processingCertificateEnvelope = metaboLightsStudyProcessorMock.processStudyInSubmission(submissionEnvelope);
+        assertEquals(processingCertificateEnvelope.getProcessingCertificates().size(), 3);
+    }
+
+
 }
