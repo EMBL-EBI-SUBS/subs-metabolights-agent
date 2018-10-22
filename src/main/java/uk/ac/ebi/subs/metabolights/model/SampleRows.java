cd8c39c5d226eb8ec8d77607777f8ee8e8fb0768
kalaij <1823024+kalaij@users.noreply.github.com>
2018-10-22 18:22:01 +0100

    Sample rows model added

---
+++ src/main/java/uk/ac/ebi/subs/metabolights/model/SampleRows.java
@@ -0,0 +1,10 @@
+package uk.ac.ebi.subs.metabolights.model;
+
+import java.util.List;
+
+/**
+ * Created by kalai on 22/10/2018.
+ */
+public class SampleRows {
+    public List<SampleMap> samples;
+}
