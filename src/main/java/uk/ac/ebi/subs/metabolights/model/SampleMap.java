32b1bc23921ba1eed184bb6d4700adbb07fff08b
kalaij <1823024+kalaij@users.noreply.github.com>
2018-10-22 18:29:46 +0100

    sample map processed from ml sample object

--- src/main/java/uk/ac/ebi/subs/metabolights/model/SampleMap.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/model/SampleMap.java
@@ -10,16 +10,17 @@ import java.util.LinkedHashMap;
 
 public class SampleMap extends LinkedHashMap {
 
-    public SampleMap(){
-        //todo take sample object and assign values
-        put(SampleSpreadSheetConstants.SOURCE_NAME,"");
-        put(SampleSpreadSheetConstants.ORGANISM,"");
-        put(SampleSpreadSheetConstants.ORGANISM_TERM_SOURCE_REF,"");
-        put(SampleSpreadSheetConstants.ORGANISM_TERM_ACCESSION_NUMBER,"");
-        put(SampleSpreadSheetConstants.ORGANISM_PART,"");
-        put(SampleSpreadSheetConstants.ORGANISM_PART_TERM_SOURCE_REF,"");
-        put(SampleSpreadSheetConstants.ORGANISM_PART_TERM_ACCESSION_NUMBER,"");
-        put(SampleSpreadSheetConstants.PROTOCOL_REF,"");
-        put(SampleSpreadSheetConstants.SAMPLE_NAME,"");
+    public SampleMap(Sample sample) {
+        //todo check for null values before assigning
+        //todo check order for organism and organism part
+        put(SampleSpreadSheetConstants.SOURCE_NAME, sample.getDerivesFrom().get(0).getName());
+        put(SampleSpreadSheetConstants.ORGANISM, sample.getDerivesFrom().get(0).getCharacteristics().get(0).getValue().getAnnotationValue());
+        put(SampleSpreadSheetConstants.ORGANISM_TERM_SOURCE_REF, sample.getDerivesFrom().get(0).getCharacteristics().get(0).getValue().getTermSource().getFile());
+        put(SampleSpreadSheetConstants.ORGANISM_TERM_ACCESSION_NUMBER, sample.getDerivesFrom().get(0).getCharacteristics().get(0).getValue().getTermAccession());
+        put(SampleSpreadSheetConstants.ORGANISM_PART, sample.getDerivesFrom().get(0).getCharacteristics().get(1).getValue().getAnnotationValue());
+        put(SampleSpreadSheetConstants.ORGANISM_PART_TERM_SOURCE_REF, sample.getDerivesFrom().get(0).getCharacteristics().get(1).getValue().getTermSource().getFile());
+        put(SampleSpreadSheetConstants.ORGANISM_PART_TERM_ACCESSION_NUMBER, sample.getDerivesFrom().get(0).getCharacteristics().get(1).getValue().getTermAccession());
+        put(SampleSpreadSheetConstants.PROTOCOL_REF, "Sample collection");
+        put(SampleSpreadSheetConstants.SAMPLE_NAME, sample.getName());
     }
 }
