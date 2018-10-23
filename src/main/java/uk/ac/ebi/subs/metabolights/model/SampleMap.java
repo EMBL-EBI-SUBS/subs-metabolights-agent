54fa3b79d7dc27e8da7bb988cdb1578069df7eaa
kalaij <1823024+kalaij@users.noreply.github.com>
2018-10-23 15:00:11 +0100

    sample map processor method added

--- src/main/java/uk/ac/ebi/subs/metabolights/model/SampleMap.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/model/SampleMap.java
@@ -22,5 +22,30 @@ public class SampleMap extends LinkedHashMap {
         put(SampleSpreadSheetConstants.ORGANISM_PART_TERM_ACCESSION_NUMBER, sample.getDerivesFrom().get(0).getCharacteristics().get(1).getValue().getTermAccession());
         put(SampleSpreadSheetConstants.PROTOCOL_REF, "Sample collection");
         put(SampleSpreadSheetConstants.SAMPLE_NAME, sample.getName());
+
+        process(sample);
+    }
+
+    private void process(Sample sample) {
+        put(SampleSpreadSheetConstants.PROTOCOL_REF, "Sample collection");
+        put(SampleSpreadSheetConstants.SAMPLE_NAME, sample.getName());
+        if (sample.getDerivesFrom().size() > 0) {
+            for (Source source : sample.getDerivesFrom()) {
+                if (source.getCharacteristics().size() > 0) {
+                    for(SampleSourceOntologyModel sourceOntologyModel : source.getCharacteristics()){
+                          if(sourceOntologyModel.getCategory().getAnnotationValue().equalsIgnoreCase("organism")){
+                              put(SampleSpreadSheetConstants.ORGANISM, sourceOntologyModel.getValue().getAnnotationValue());
+                              put(SampleSpreadSheetConstants.ORGANISM_TERM_SOURCE_REF, sourceOntologyModel.getValue().getTermSource().getFile());
+                              put(SampleSpreadSheetConstants.ORGANISM_TERM_ACCESSION_NUMBER, sourceOntologyModel.getValue().getTermAccession());
+                          }
+                        if(sourceOntologyModel.getCategory().getAnnotationValue().equalsIgnoreCase("organism part")){
+                            put(SampleSpreadSheetConstants.ORGANISM_PART, sourceOntologyModel.getValue().getAnnotationValue());
+                            put(SampleSpreadSheetConstants.ORGANISM_PART_TERM_SOURCE_REF, sourceOntologyModel.getValue().getTermSource().getFile());
+                            put(SampleSpreadSheetConstants.ORGANISM_PART_TERM_ACCESSION_NUMBER, sourceOntologyModel.getValue().getTermAccession());
+                        }
+                    }
+                }
+            }
+        }
     }
 }
