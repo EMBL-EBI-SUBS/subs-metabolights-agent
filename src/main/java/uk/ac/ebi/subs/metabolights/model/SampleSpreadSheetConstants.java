762f2a8622afa19e41a450b9b34e3629bb56cc4e
kalaij <1823024+kalaij@users.noreply.github.com>
2018-10-25 22:33:20 +0100

    Sample Organism variant added to model

--- src/main/java/uk/ac/ebi/subs/metabolights/model/SampleSpreadSheetConstants.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/model/SampleSpreadSheetConstants.java
@@ -10,9 +10,12 @@ public class SampleSpreadSheetConstants {
     public static final String ORGANISM = "Characteristics[Organism]";
     public static final String ORGANISM_TERM_SOURCE_REF = "Term Source REF";
     public static final String ORGANISM_TERM_ACCESSION_NUMBER = "Term Accession Number";
+    public static final String VARIANT = "Characteristics[Variant]";
+    public static final String VARIANT_TERM_SOURCE_REF = "Term Source REF.1";
+    public static final String VARIANT_TERM_ACCESSION_NUMBER = "Term Accession Number.1";
     public static final String ORGANISM_PART = "Characteristics[Organism part]";
-    public static final String ORGANISM_PART_TERM_SOURCE_REF = "Term Source REF.1";
-    public static final String ORGANISM_PART_TERM_ACCESSION_NUMBER = "Term Accession Number.1";
+    public static final String ORGANISM_PART_TERM_SOURCE_REF = "Term Source REF.2";
+    public static final String ORGANISM_PART_TERM_ACCESSION_NUMBER = "Term Accession Number.2";
     public static final String PROTOCOL_REF = "Protocol REF";
     public static final String SAMPLE_NAME = "Sample Name";
 }
