827feadf24d0d66d27f1212c557f6b6577d80ae8
kalaij <1823024+kalaij@users.noreply.github.com>
2018-11-15 23:40:44 +0000

    assay map process method added

--- src/main/java/uk/ac/ebi/subs/metabolights/model/AssayMap.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/model/AssayMap.java
@@ -1,4 +1,18 @@
 package uk.ac.ebi.subs.metabolights.model;
 
-public class AssayMap {
+import java.util.LinkedHashMap;
+
+/**
+ * Created by kalai on 22/10/2018.
+ */
+
+public class AssayMap extends LinkedHashMap<String, String> {
+
+    public AssayMap(Assay assay) {
+        super();
+        process(assay);
+    }
+
+    private void process(Assay assay) {
+    }
 }
