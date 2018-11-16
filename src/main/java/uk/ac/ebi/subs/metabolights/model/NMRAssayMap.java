556100e23eca3dcc84c94fef7eda5c0b8097be41
kalaij <1823024+kalaij@users.noreply.github.com>
2018-11-16 15:33:08 +0000

    NMR sample protocol placeholder values added

--- src/main/java/uk/ac/ebi/subs/metabolights/model/NMRAssayMap.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/model/NMRAssayMap.java
@@ -20,6 +20,26 @@ public class NMRAssayMap extends LinkedHashMap<String, String> {
         put(AssaySpreadSheetConstants.EXTRACTION_PROTOCOL_EXTRACTION_METHOD,"");
         put(AssaySpreadSheetConstants.EXTRACTION_PROTOCOL_EXTRACT_NAME,"");
 
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_REF,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TUBE_TYPE ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TUBE_TYPE_TSR ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TUBE_TYPE_TAN ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_SOLVENT ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_SOLVENT_TSR ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_SOLVENT_TAN ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_SAMPLE_PH ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TEMPERATURE ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_UNIT,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TEMPERATURE_UNIT_TSR ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TEMPERATURE_UNIT_TAN ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_EXTRACT_NAME ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_LABEL,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_EXTRACT_NAME_TSR ,"");
+        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_EXTRACT_NAME_TAN ,"");
+
+        
+
+
 
     }
 }
