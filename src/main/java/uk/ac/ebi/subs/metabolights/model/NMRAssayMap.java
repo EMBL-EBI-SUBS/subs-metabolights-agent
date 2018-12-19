bf09d424045f2f6e233733b0858782bb074e81bd
kalaij <1823024+kalaij@users.noreply.github.com>
2018-12-19 15:23:12 +0000

    NMR assay and NMR spectroscopy protocols refactored to nmr assay map

--- src/main/java/uk/ac/ebi/subs/metabolights/model/NMRAssayMap.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/model/NMRAssayMap.java
@@ -195,10 +195,90 @@ public class NMRAssayMap extends AssayMap {
         }
     }
 
-    private void parseNMRSpectroscopy(ProtocolUse protocolUse) {
+    private void parseNMRSpectroscopy(ProtocolUse nmrSpectroscopy) {
+        this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_REF, "NMR spectroscopy");
+        if (nmrSpectroscopy.getAttributes().size() > 0) {
+            if (nmrSpectroscopy.getAttributes().containsKey("Instrument")) {
+                Attribute instrument = nmrSpectroscopy.getAttributes().get("Instrument").iterator().next();
+                if (instrument != null && instrument.getValue() != null) {
+                    this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_INSTRUMENT, instrument.getValue());
+                    if (instrument.getTerms() != null && instrument.getTerms().size() == 1) {
+                        if (instrument.getTerms().get(0).getUrl() != null || !instrument.getTerms().get(0).getUrl().isEmpty()) {
+                            this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_INSTRUMENT_TSR, instrument.getTerms().get(0).getUrl());
+                            this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_INSTRUMENT_TAN, "");
+                            //todo term accession number not set
+                        }
+                    }
+                }
+            }
+            if (nmrSpectroscopy.getAttributes().containsKey("NMR Probe")) {
+                Attribute nmr_Probe = nmrSpectroscopy.getAttributes().get("NMR Probe").iterator().next();
+                if (nmr_Probe != null && nmr_Probe.getValue() != null) {
+                    this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_NMR_PROBE, nmr_Probe.getValue());
+                    if (nmr_Probe.getTerms() != null && nmr_Probe.getTerms().size() == 1) {
+                        if (nmr_Probe.getTerms().get(0).getUrl() != null || !nmr_Probe.getTerms().get(0).getUrl().isEmpty()) {
+                            this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_NMR_PROBE_TSR, nmr_Probe.getTerms().get(0).getUrl());
+                            this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_NMR_PROBE_TAN, "");
+                            //todo term accession number not set
+                        }
+                    }
+                }
+            }
+
+            if (nmrSpectroscopy.getAttributes().containsKey("Number of transients")) {
+                Attribute transients = nmrSpectroscopy.getAttributes().get("Number of transients").iterator().next();
+                if (transients != null && transients.getValue() != null) {
+                    this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_NO_OF_TRANSIENTS, transients.getValue());
+                }
+            }
+
+            if (nmrSpectroscopy.getAttributes().containsKey("Pulse sequence name")) {
+                Attribute pulse_sequence_name = nmrSpectroscopy.getAttributes().get("Pulse sequence name").iterator().next();
+                if (pulse_sequence_name != null && pulse_sequence_name.getValue() != null) {
+                    this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_PULSE_SEQ_NAME, pulse_sequence_name.getValue());
+                }
+            }
+
+            if (nmrSpectroscopy.getAttributes().containsKey("Magnetic field strength")) {
+                Attribute magnetic_field_strength = nmrSpectroscopy.getAttributes().get("Magnetic field strength").iterator().next();
+                if (magnetic_field_strength != null && magnetic_field_strength.getValue() != null) {
+                    this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_MAGNETIC_FIELD_STRENGTH, magnetic_field_strength.getValue());
+
+                    if (magnetic_field_strength.getUnits() != null && !magnetic_field_strength.getUnits().isEmpty()) {
+                        this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_UNIT, magnetic_field_strength.getUnits());
+                        this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_UNIT_TSR, "");
+                        this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_UNIT_TAN, "");
+                        //todo term accession number and term source not set for unit
+                    }
+                }
+            }
+
+            if (nmrSpectroscopy.getAttributes().containsKey("Acquisition Parameter Data File")) {
+                Attribute acquisition_parameter_data_file = nmrSpectroscopy.getAttributes().get("Acquisition Parameter Data File").iterator().next();
+                if (acquisition_parameter_data_file != null && acquisition_parameter_data_file.getValue() != null) {
+                    this.put(AssaySpreadSheetConstants.NMR_PROTOCOL_ACQUISITION_PM_DATA_FILE, acquisition_parameter_data_file.getValue());
+                }
+            }
+        }
+
     }
 
-    private void parseNMRAssay(ProtocolUse protocolUse) {
+    private void parseNMRAssay(ProtocolUse nmrAssay) {
+        this.put(AssaySpreadSheetConstants.NMR_ASSAY_PROTOCOL_REF, "NMR assay");
+        if (nmrAssay.getAttributes().size() > 0) {
+            if (nmrAssay.getAttributes().containsKey("NMR Assay Name")) {
+                Attribute nmr_assay_name = nmrAssay.getAttributes().get("NMR Assay Name").iterator().next();
+                if (nmr_assay_name != null && nmr_assay_name.getValue() != null) {
+                    this.put(AssaySpreadSheetConstants.NMR_ASSAY_PROTOCOL_NAME, nmr_assay_name.getValue());
+                }
+            }
+            if (nmrAssay.getAttributes().containsKey("Free Induction Decay Data File")) {
+                Attribute free_induction_decay_data_file = nmrAssay.getAttributes().get("Free Induction Decay Data File").iterator().next();
+                if (free_induction_decay_data_file != null && free_induction_decay_data_file.getValue() != null) {
+                    this.put(AssaySpreadSheetConstants.NMR_ASSAY_FID_FILE, free_induction_decay_data_file.getValue());
+                }
+            }
+        }
     }
 
     private void parseDataTransformation(ProtocolUse protocolUse) {
