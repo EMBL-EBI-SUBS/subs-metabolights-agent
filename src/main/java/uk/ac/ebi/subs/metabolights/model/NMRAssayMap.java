b53421d25a152b2615c079412d7e02c316cc3992
kalaij <1823024+kalaij@users.noreply.github.com>
2018-12-24 21:36:10 +0000

    NMR assay map method refactoring

--- src/main/java/uk/ac/ebi/subs/metabolights/model/NMRAssayMap.java
+++ src/main/java/uk/ac/ebi/subs/metabolights/model/NMRAssayMap.java
@@ -24,28 +24,7 @@ public class NMRAssayMap extends AssayMap {
           parseSample(assay.getSampleUses());
           parseProtocol(assay.getProtocolUses());
 
-//        put(AssaySpreadSheetConstants.SAMPLE_NAME, "");
-//
-//        put(AssaySpreadSheetConstants.EXTRACTION_PROTOCOL_REF,"");
-//        put(AssaySpreadSheetConstants.EXTRACTION_PROTOCOL_EXTRACTION_METHOD,"");
-//        put(AssaySpreadSheetConstants.EXTRACTION_PROTOCOL_EXTRACT_NAME,"");
-//
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_REF,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TUBE_TYPE ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TUBE_TYPE_TSR ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TUBE_TYPE_TAN ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_SOLVENT ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_SOLVENT_TSR ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_SOLVENT_TAN ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_SAMPLE_PH ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TEMPERATURE ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_UNIT,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TEMPERATURE_UNIT_TSR ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_TEMPERATURE_UNIT_TAN ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_EXTRACT_NAME ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_LABEL,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_EXTRACT_NAME_TSR ,"");
-//        put(AssaySpreadSheetConstants.NMR_SAMPLE_PROTOCOL_EXTRACT_NAME_TAN ,"");
+
 //
 //        put(AssaySpreadSheetConstants.NMR_PROTOCOL_REF ,"");
 //        put(AssaySpreadSheetConstants.NMR_PROTOCOL_INSTRUMENT ,"");
