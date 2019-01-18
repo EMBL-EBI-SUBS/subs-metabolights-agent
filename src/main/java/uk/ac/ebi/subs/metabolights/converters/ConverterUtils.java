aeecc1872aa85a30a90ad53641cdbee84a9fa5e9
kalaij <1823024+kalaij@users.noreply.github.com>
2019-01-18 16:45:05 +0000

    Converter util method added

---
+++ src/main/java/uk/ac/ebi/subs/metabolights/converters/ConverterUtils.java
@@ -0,0 +1,31 @@
+package uk.ac.ebi.subs.metabolights.converters;
+
+import uk.ac.ebi.subs.data.component.Attribute;
+import uk.ac.ebi.subs.metabolights.model.Comment;
+import uk.ac.ebi.subs.metabolights.model.SampleSpreadSheetConstants;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class ConverterUtils {
+
+
+    public static List<Comment> convertIndexInfoToComments(Map<String, Collection<Attribute>> usiAttributes) {
+        List<Comment> comments = new ArrayList<>();
+        for (Map.Entry<String, Collection<Attribute>> entry : usiAttributes.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase(SampleSpreadSheetConstants.ROW_INDEX)) {
+                if (entry.getValue().size() > 0) {
+                    Attribute attribute = entry.getValue().iterator().next();
+                    Comment comment = new Comment();
+                    comment.setName(SampleSpreadSheetConstants.ROW_INDEX);
+                    comment.setValue(attribute.getValue());
+                    comments.add(comment);
+                }
+            }
+
+        }
+        return comments;
+    }
+}
